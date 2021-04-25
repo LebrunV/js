b0008e2f580e70d9f3fee11817f651b1e7e2d769
Vincent Lebrun <lebrunv@mail.be>
2021-04-25 17:55:00 +0200

    Pratique04 - Horloge

---
+++ Pratique04 - horloge/index.js
@@ -0,0 +1,13 @@
+function changeHeure(){
+    var date = new Date()
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    if( h < 10 ){ h = '0' + h; }
+    if( m < 10 ){ m = '0' + m; }
+    if( s < 10 ){ s = '0' + s; }
+    var time = h + ':' + m + ':' + s
+    document.getElementById('horloge').innerHTML = time;
+    var t = 1000;
+    setTimeout('changeHeure()',t)
+}
